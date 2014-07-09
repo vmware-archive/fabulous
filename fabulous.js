d0ccb49ea2aa751f1e7f58b77f3759baafdd7134
Brian Cavalier <brian@hovercraftstudios.com>
2014-07-09 11:57:16 -0400

    Remove navigation for now, stay focused on observable and sync

--- fabulous.js
+++ fabulous.js
@@ -1,10 +1,9 @@
 var sync = require('./context/sync');
 var scan = require('./context/scan');
-var nav = require('./context/nav');
 var curry = require('./lib/fn').curry;
 
 exports.run = curry(run);
 
 function run(node, builder, context) {
-	return nav(sync(scan(node, builder)), context);
+	return sync(scan(node, builder), context);
 }
